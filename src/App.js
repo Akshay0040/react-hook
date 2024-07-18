ad02dfe0f0ac11dabf476e9c094950d5fdc3eb12
Akshay yadav <akshayyadav9627@gmail.com>
2024-07-18 18:51:48 +0530

    hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  let [counter, setCounter] = useState(15)
+
+  //let counter = 15
+
+  const addValue = () => {
+    console.log('clicked', counter); 
+    if (counter < 20) {
+    setCounter(counter + 1)
+    }
+  };
+
+  const removeValue = () => {
+    if (counter > 0) {
+    setCounter(counter - 1)
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello Akshay</h1>
+    <h2>Counter Value: {counter}</h2>
+
+    <button onClick={addValue}> Add value {counter}</button>
+
+    <br/>
+
+    <button onClick={removeValue}> Remove value {counter}</button>
+
+    <p>footer: {counter}</p>
+
+    </>
   );
 }
 
